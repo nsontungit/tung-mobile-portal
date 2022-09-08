87e97be5fdb1ec39acce7fa3f99e6d1011720e46
nstung7 <97228470+Nguyen-Son-Tung@users.noreply.github.com>
2022-09-09 00:18:28 +0700

    Environment Varibles

--- main/src/app/services/laptop.service.ts
+++ main/src/app/services/laptop.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { ApiResult } from '../models/api-result';
 import { Paging } from '../models/common';
 import { Laptop, LaptopDto } from '../models/laptop';
@@ -12,36 +13,37 @@ import { Option } from '../models/option';
 })
 export class LaptopService {
 
-  private host_url: string = 'http://localhost:5000/api/laptops';
+  private laptop_suffix: string = 'api/laptops';
+  private product_url: string = `${environment.productUrl}`;
 
   constructor(private http: HttpClient) { }
 
   getOne() {
-    return this.http.get<Laptop>(`${this.host_url}`)
+    return this.http.get<Laptop>(`${this.product_url}/${this.laptop_suffix}`)
         .pipe(map (e => e));
   }
 
   getAll(pageSize: number, pageNumber: number): Observable<Paging<Laptop>> {
-    return this.http.get<ApiResult>(`${this.host_url}?pageSize=${pageSize}&pageNumber=${pageNumber}`)
+    return this.http.get<ApiResult>(`${this.product_url}/${this.laptop_suffix}?pageSize=${pageSize}&pageNumber=${pageNumber}`)
         .pipe(map (e => e.data as Paging<Laptop>));
   }
 
   getOptions(type: string): Observable<Option[]> {
-    return this.http.get<ApiResult>(`${this.host_url}/options/${type}`)
+    return this.http.get<ApiResult>(`${this.product_url}/${this.laptop_suffix}/options/${type}`)
         .pipe(map (e => e.data as Option[]));
   }
 
   getLaptopBrandOptions(): Observable<Option[]> {
-    return this.http.get<ApiResult>(`${this.host_url}/options/laptop-brand`)
+    return this.http.get<ApiResult>(`${this.product_url}/${this.laptop_suffix}/options/laptop-brand`)
         .pipe(map (e => e.data as Option[]));
   }
 
   getAllOptions(): Observable<Option[]> {
-    return this.http.get<ApiResult>(`${this.host_url}/options`)
+    return this.http.get<ApiResult>(`${this.product_url}/${this.laptop_suffix}/options`)
         .pipe(map (e => e.data as Option[]));
   }
 
   createOne(formData): Observable<ApiResult> {
-    return this.http.post<ApiResult>(`${this.host_url}`, formData);
+    return this.http.post<ApiResult>(`${this.product_url}/${this.laptop_suffix}`, formData);
   }
 }
